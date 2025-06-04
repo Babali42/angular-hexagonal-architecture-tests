e7004e3041ab15a0d6323ffabcb1d33e5413faad
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2025-06-04 13:58:27 +0200

    add business context test

---
+++ src/app/package-contexts.ts
@@ -0,0 +1,4 @@
+export enum BusinessContext {
+  BILLING = 'billing',
+  SUBSCRIPTION = 'subscription',
+}
--- src/architecture-test/architecture.spec.jest.ts
+++ src/architecture-test/architecture.spec.jest.ts
@@ -1,6 +1,7 @@
 import {TypeScriptProject} from "arch-unit-ts/dist/arch-unit/core/domain/TypeScriptProject";
 import {RelativePath} from "arch-unit-ts/dist/arch-unit/core/domain/RelativePath";
-import {classes} from "arch-unit-ts/dist/main";
+import {classes, noClasses} from "arch-unit-ts/dist/main";
+import {BusinessContext} from '../app/package-contexts';
 
 describe('Hexagonal architecture test', () => {
   const srcProject = new TypeScriptProject(RelativePath.of("src"));
@@ -15,6 +16,33 @@ describe('Hexagonal architecture test', () => {
         .resideInAPackage('..domain..')
         .because('domain model should only depends on domain')
         .check(srcProject.allClasses())
-    })
-  })
+    });
+  });
+
+  const businessContext = packageWithContext(BusinessContext.SUBSCRIPTION)
+    .concat(packageWithContext(BusinessContext.BILLING));
+
+  function otherBusinessContextsDomain(context: string): string[] {
+    return [BusinessContext.SUBSCRIPTION, BusinessContext.BILLING].filter(other => other != context).map(name => name + '.domain');
+  }
+
+  function packageWithContext(contextName: string): string[] {
+    return srcProject
+      .filterClasses('**.package-info.ts')
+      .filter(typescriptClass => typescriptClass.hasImport(contextName))
+      .map(typescriptClass => typescriptClass.packagePath.getDotsPath());
+  }
+
+  describe("Business contexts", () => {
+    it.each([BusinessContext.SUBSCRIPTION, BusinessContext.BILLING])("Context '%s' should not depend on other bounded contexts", (context) => {
+      noClasses()
+        .that()
+        .resideInAPackage(context + '..')
+        .should()
+        .dependOnClassesThat()
+        .resideInAPackage(...otherBusinessContextsDomain(context))
+        .because("Bounded contexts should be isolated")
+        .check(srcProject.allClasses());
+    });
+  });
 });
